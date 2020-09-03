2f6ea0f606084d2aa077af7521ec0170b432e445
DIANA ALAZZAM <DAlAzzam@aspire-services.net>
2020-09-03 11:23:10 +0300

    started with auth, sign up complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import TasksList from './components/list';
-
+import Login from './components/auth/login';
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -15,6 +15,11 @@ function App() {
     setTasks(newList);
     localStorage.setItem('Tasks List', newList)
   }
+  const showTasks = (userTasks)=> {
+    console.log('hello')
+    setTasks(userTasks)
+  };
+
   useEffect(()=> {
     let localStorageTasksList = localStorage.getItem('Tasks List');
     if (localStorageTasksList) setTasks(localStorageTasksList.split(','));
@@ -22,8 +27,9 @@ function App() {
 
   return (
     <div id='app'>
-      <Header/>
+      <Header showTasks={showTasks}/>
       <Form addTask={addTask}/>
+      {/* add condition and show not found if there we no tasks */}
       <TasksList tasksList={tasks}/>
       <Footer/>
     </div>
---
+++ src/components/auth/signup.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+function SignupModal(props) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [usersState, setUsers] = useState([]);
+
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const formHandler = (e) => {
+        e.preventDefault();
+
+        const { username, password } = e.target;
+        let usersList = usersState.slice();
+        usersList.push(`${username.value} ${password.value}`);
+        setUsers(usersList);
+        localStorage.setItem('users', usersList);
+        e.target.reset();
+        handleClose();
+    }
+    useEffect(() => {
+        let savedUsers = localStorage.getItem('users');
+        if (savedUsers) setUsers(savedUsers);
+    }, [])
+    return (
+        <div>
+            <button type="button" onClick={handleOpen}>
+                Sign-up
+            </button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+                style={{ backgroundColor: 'white' }}
+            >
+                <Fade in={open}>
+                    <div id='modal-box'>
+                        <form onSubmit={formHandler} id='login-form'>
+                            <p>Please Sign-up to add new tasks</p>
+                            <TextField required label="Username" name='username' variant="outlined" />
+                            <TextField required type='password' label="Password" name='password' variant="outlined" />
+                            <Button variant="contained" color="primary" type='submit'>
+                                Sign-up
+                            </Button>
+                        </form>
+                    </div>
+                </Fade>
+            </Modal>
+        </div>
+    )
+}
+
+export default SignupModal;
